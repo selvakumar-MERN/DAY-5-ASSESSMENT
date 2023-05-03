67da096440819d3a7a03d80ca82468295f2976e0
selvakumar-MERN <selva81222@gmail.com>
2023-05-03 16:02:05 +0530

    fifth commit

---
+++ QUESTION-2.js
@@ -0,0 +1,43 @@
+//CREATE YOUR OWN RESUME DATA IN JSON FORMAT
+
+var resume={
+    "name":"selvakumar",
+     "age":"25",
+     "gender":"male",
+     "mother tongue":"tamil",
+     "medium of schooling":"tamil",
+     "nationality":"indian",
+   "qualification":{
+    "sslc":{
+      "institution":"karthik vidhya mandir",
+      "eduction":"sslc",
+      "year":"2014",
+      "percentage":"87.6%"},
+    "hsc":{
+      "institution":"karthik vidhya mandir",
+      "eduction":"hsc",
+      "year":"2014",
+      "percentage":"87.25%"},
+    "degree":{
+      "institution":"kaarunya university",
+      "eduction":"b.tech mechanical engineering",
+      "year":"2018",
+      "percentage":"78.3%"}
+    },
+"training and certificates": ["cambridge english entry level certificate",
+                              "supra sae india"],
+ "project":"productivity improvement in condenser manifold at wonjin autoparts",
+ "experience":{
+    "industry":"wonjin autoparts pvt.ltd",
+    "role":"junior engineer",
+    "year":"4"
+ }, 
+ "skills":["solidworks desgin","ansys analysis"],
+ "address":["193, south anna nagar, pattali street,palani"],
+ "email":"selva81222@gmail.com",
+ "mobile no":"6381502935"                            
+ 
+
+}
+console.log(resume);
+//OUTPUT: IMAGE ADDED.
